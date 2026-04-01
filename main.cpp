4886d164e20f4724a76f75a371b68a79e2427d0e
jawid <jawidbay@gmail.com>
2026-04-01 12:28:52 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,149 +1,168 @@
 #include<iostream>
-#include<string>
+
 using namespace std;
 
-struct song{
-    string title;
-    string artist;
-    int ratings[3];
-    song* next;
-    song* prev;
+struct package {
+
+    string packageID;
+    string distinationCity;
+    int dimensions[3];
+    package *next;
+
 };
 
-song* head = nullptr;
-song* tail = nullptr;
-song* curr = nullptr;
 
-void addSong(string title, string artist, int r1, int r2, int r3){
-    song* new_node = new song();
-    new_node->title = title;
-    new_node->artist = artist;
-    new_node->ratings[0] = r1;
-    new_node->ratings[1] = r2;
-    new_node->ratings[2] = r3;
-    new_node->next = nullptr;
-    new_node->prev = nullptr;
+package* Incoming_Cargo(package *head, string City, string ID, int arr[3]){
+
+    package *new_Node = new package;
+
+    new_Node->distinationCity = City;
+    new_Node->packageID = ID;
+    new_Node->dimensions[0] = arr[0];
+    new_Node->dimensions[1] = arr[1];
+    new_Node->dimensions[2] = arr[2];
+    new_Node->next = nullptr;
+
+    new_Node->next = head;
+    return new_Node;
+}
+
+
+void travers_print(package *head){
 
     if (head == nullptr){
-        head = new_node;
-        tail = new_node;
-        curr = new_node;
-    }
-    else{
-        new_node->prev = tail;
-        tail->next = new_node;
-        tail = new_node;
-    }
-}
-
-void nextSong() {
-    if (curr != nullptr && curr->next != nullptr) {
-        curr = curr->next;
-    }
-}
-
-void prevSong() {
-    if (curr != nullptr && curr->prev != nullptr) {
-        curr = curr->prev;
-    }
-}
-
-void removeCurrent(){
-    if (curr == nullptr) return;
-
-    song *toDelete = curr;
-
-    if (toDelete->prev != nullptr){
-        toDelete->prev->next = toDelete->next;
-    }
-    else{
-        head = toDelete->next;
-    }
-
-    if (toDelete->next != nullptr){
-        toDelete->next->prev = toDelete->prev;
-        curr = toDelete->next;
-    }
-    else {
-        tail = toDelete->prev;
-        curr = tail;
-    }
-
-    delete toDelete;
-
-    if (head == nullptr) {
-        curr = nullptr;
-    }
-}
-
-void displayPlaylist(){
-    if (head == nullptr) {
-        cout << "Playlist is empty!\n";
+        cout << "List is empty\n";
         return;
     }
-
-    song *temp = head;
+    package *temp = head;
     while(temp != nullptr){
-        if (temp == curr){
-            cout << "-> ";
-        } else {
-            cout << "   ";
-        }
-        cout << temp->title << " by " << temp->artist << endl;
-        cout << "Ratings: ";
-        for (int i = 0; i < 3; i++){
-            cout << temp->ratings[i];
-            if (i < 2) cout << ", ";
-        }
-        cout << endl << endl;
+        cout << "ID: " << temp->packageID
+             << ", City: " << temp->distinationCity
+             << ", Dimensions: "
+             << temp->dimensions[0] << " x "
+             << temp->dimensions[1] << " x "
+             << temp->dimensions[2]
+             << endl;
         temp = temp->next;
     }
 }
+package* Pop(package* head) {
+    if (head == nullptr) {
+        cout << "No package to pop.\n";
+        return nullptr;
+    }
+
+    package* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+struct Courier {
+    string courierName;
+    string vehicleType;
+};
+
+const int SIZE = 5;
+Courier courierQueue[SIZE];
+int front = -1, rear = -1;
+
+void Enqueue(string name, string vehicle){
+    if(front == -1){ front = 0; rear = 0; }
+    else { rear = (rear + 1) % SIZE; }
+    courierQueue[rear].courierName = name;
+    courierQueue[rear].vehicleType = vehicle;
+    cout << name << " added to queue.\n";
+}
+
+void Dequeue(){
+    cout << "Courier dispatched -> " << courierQueue[front].courierName
+         << ", Vehicle: " << courierQueue[front].vehicleType << "\n";
+    if(front == rear){ front = -1; rear = -1; }
+    else { front = (front + 1) % SIZE; }
+}
+
+void printQueue(){
+    if(front == -1){ cout << "Queue is empty!\n"; return; }
+    int i = front;
+    while(true){
+        cout << "Name: " << courierQueue[i].courierName
+             << ", Vehicle: " << courierQueue[i].vehicleType << "\n";
+        if(i == rear) break;
+        i = (i + 1) % SIZE;
+    }
+}
+
+
+
 
 int main(){
-    addSong("Unuttun mu?", "Sezen Aksu", 5, 4, 5);
-    addSong("Hosca kal", "Emre Aydin", 3, 2, 4);
-    addSong("Unutulacak Dunler", "Gazapizm", 1, 2, 5);
+    package *head = nullptr;
+    int choice;
 
-    int choice = 0;
-    while (choice != 5) {
-        displayPlaylist();
-        cout << "1.Next Song\n";
-        cout << "2.Previous Song\n";
-        cout << "3.Remove Current\n";
-        cout << "4.Add New Song\n";
-        cout << "5.Exit\n";
+    while(true){
+        cout << "\n       MENU        \n";
+        cout << "1. Arrival\n";
+        cout << "2. Courier Register\n";
+        cout << "3. Dispatch\n";
+        cout << "4. Display\n";
+        cout << "5. Exit\n";
         cout << "Choose: ";
         cin >> choice;
-        cin.ignore();
 
-        if(choice == 1){
-            nextSong();
-        }
-        else if(choice == 2){
-            prevSong();
-        }
-        else if(choice == 3){
-            removeCurrent();
-        }
-        else if(choice == 4){
-            string title, artist;
-            int r1, r2, r3;
-            cout << "Enter song title: ";
-            getline(cin, title);
-            cout << "Enter artist name: ";
-            getline(cin, artist);
-            cout << "Enter 3 ratings: ";
-            cin >> r1 >> r2 >> r3;
-            cin.ignore();
-            addSong(title, artist, r1, r2, r3);
-        }
-        else if(choice == 5){
-            cout << "Exiting...\n";
-        }
-        else {
-            cout << "Invalid choice!\n";
+        switch(choice){
+
+            case 1: {
+                string city, id;
+                int dimensions[3];
+                cout << "Enter destination City: ";  cin >> city;
+                cout << "Enter ID: ";                cin >> id;
+                cout << "Enter dimensions (Length, Width, Height): ";
+                cin >> dimensions[0] >> dimensions[1] >> dimensions[2];
+                head = Incoming_Cargo(head, city, id, dimensions);
+                break;
+            }
+
+            case 2: {
+                string name, vehicle;
+                cout << "Enter courier name: ";   cin >> name;
+                cout << "Enter vehicle type: ";   cin >> vehicle;
+                Enqueue(name, vehicle);
+                break;
+            }
+
+            case 3: {
+                cout << "\n--- DISPATCH ---\n";
+                if (head == nullptr) {
+                    cout << "No packages to dispatch.\n";
+                } else {
+                    cout << "Courier " << courierQueue[front].courierName
+                         << " is picking up package " << head->packageID << ".\n";
+                    head = Pop(head);
+                }
+                Dequeue();
+                break;
+            }
+
+            case 4: {
+                cout << "\n-- Packages --\n";
+                travers_print(head);
+                cout << "\n-- Couriers --\n";
+                printQueue();
+                break;
+            }
+
+            case 5: {
+                package* temp;
+                while(head != nullptr){
+                    temp = head;
+                    head = head->next;
+                    delete temp;
+                }
+                return 0;
+            default:
+                cout << "Invalid Input\n";
+            }
         }
     }
-    return 0;
 }
